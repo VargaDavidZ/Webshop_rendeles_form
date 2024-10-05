b0a3299009d0ede9b500422c1e7c3f7307f69e1b
VargaDavidZ <varga.zo.david@gmail.com>
2024-10-05 22:25:17 +0200

    fixed main page input value wont delet efter incorrect input

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { query, Response, response } from 'express';
+import { newAccountDto } from './newAccount.dto';
+
+
 
 @Controller()
 export class AppController {
@@ -12,4 +16,94 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+
+  @Get("mainPage")
+  @Render("mainPage")
+  getMain(){
+    
+  }
+
+
+
+  #accounts = []
+  
+  
+  
+  
+
+  @Get("inputDetails")
+  @Render("inputDetailsForm")
+  inputDetailsForm(@Query('item') item:string)
+  {
+        return{
+          err: [],
+          data: {}
+        }
+  }
+
+
+  @Post("inputDetails")
+  newInput(@Body() accoundData: newAccountDto, @Res() response: Response)
+  {
+
+
+    let err = [];
+
+    let newAccount = {
+      name: accoundData.name,
+      billingAddress: accoundData.billingAddress,
+      delAddress: accoundData.delAddress,
+      cupon: accoundData.cupon,
+      cardNum: accoundData.cardNum,
+      expDate: accoundData.expDate,
+      secCode: accoundData.secCode
+    }
+
+
+    if(!accoundData.name || !accoundData.billingAddress || !accoundData.delAddress || !accoundData.cardNum || !accoundData.cupon || !accoundData.expDate || !accoundData.secCode) {
+      err.push("Minden mezőt kötelező megadni!")
+    }
+
+    if(!/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(accoundData.cardNum)){
+      err.push('A kártyaszám nem megfelelő formátomú!')
+    }
+
+    if(!/^\d{2}\d{2}$/.test(accoundData.expDate)){
+      err.push('A lejárati dátum nem megfelelő')
+    }
+
+    if(err.length > 0)
+      {
+         response.render('inputDetailsForm', {
+          err,
+          data: accoundData
+ 
+        })
+        return
+      }
+  
+
+
+    this.#accounts.push(newAccount)
+
+    response.redirect(303,"paymentSuccess")
+    console.log(newAccount.billingAddress)
+
+    
+  }
+
+
+  @Get('paymentSuccess')
+  @Render("paymentSuccess")
+  newAccountSuccess() {
+    return {
+      accounts: this.#accounts.length
+    }
+  }
+
+
+
+
+
 }
---
+++ src/newAccount.dto.ts
@@ -0,0 +1,12 @@
+export class newAccountDto{
+
+    name: string;
+    billingAddress: string;
+    delAddress: string;
+    cupon: string;
+    cardNum: string;
+    expDate: string;
+    secCode: string;
+
+
+}
